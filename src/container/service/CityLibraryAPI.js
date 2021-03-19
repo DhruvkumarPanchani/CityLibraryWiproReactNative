67c228063ae476730c361cdf941478d513557ecf
--replace-all <panchanidhruv28@yahoo.com>
2021-03-19 10:00:09 +0530

    Initial commit

---
+++ src/container/service/CityLibraryAPI.js
@@ -0,0 +1,101 @@
+// set API
+var apiEndPoint = " http://training.pyther.com:3000/api/book";
+
+// Get all book List
+export var getBook = () =>{
+    return fetch(apiEndPoint, {
+        method: 'get',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+              }
+        })
+      .then(response => response.json())
+      .then(response => {
+        return(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Get book Count
+export var getBookCount = () =>{
+  return fetch(apiEndPoint, {
+      method: 'get',
+          headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+            }
+      })
+    .then(response => response.json())
+    .then(response => {
+      return(response.length);
+      }).catch(function(error) {
+          console.log(error);
+      });
+}
+
+// Add Book
+export var addBook = (customer) =>{
+    return fetch(apiEndPoint, {
+        method: 'post',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+              },
+              body:JSON.stringify(customer)
+        })
+      .then(response => response.json())
+      .then(response => {
+        return(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Delete Book
+export var deleteBook = (customer) =>{
+    return fetch(apiEndPoint, {
+        method: 'delete',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+              },
+              body:JSON.stringify(customer)
+        })
+      .then(response => response.json())
+      .then(response => {
+        return(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Get Book by Id
+export var getBookById =  (id) =>{
+    return fetch(apiEndPoint+"/"+id, {
+        method: 'get',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+              }
+        })
+      .then(response => response.json())
+      .then(response => {
+        return(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Update Book
+export var updateBook = (customer) =>{
+    return fetch(apiEndPoint, {
+        method: 'put',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+              },
+              body:JSON.stringify(customer)
+        })
+      .then(response => response.json())
+      .then(response => {
+        return(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
